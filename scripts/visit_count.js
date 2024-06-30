3478407f1a1b0a4f7371e83a2eda52a94eb9c17f
geoenterprise <jovannyrey@hotmail.com>
2024-06-30 00:51:45 -0500

    Team activity complete

--- scripts/visit_count.js
+++ scripts/visit_count.js
@@ -1,5 +1,5 @@
 // 1️⃣ Initialize display element variable
-const visitsDisplay = document.querySelector(".visit-count");
+const visitsDisplay = document.querySelector("#visit-count");
 
 // 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
