3603004ccb878eb4a40aed7491a710712a54d4c9
geoenterprise <jovannyrey@hotmail.com>
2024-07-04 01:16:44 -0500

    week 3 chamber update

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,44 @@
+const optionsYear = {year: 'numeric'};
+document.getElementById('year').textContent = new Date().toLocaleDateString(undefined, optionsYear);
+
+let lastModified = document.lastModified;
+const lastModifiedElement = document.getElementById('last-modified');
+// lastModifiedElement.textContent = `Last modified: ${lastModified}`;
+
+document.getElementById('last-modified').textContent = `Last modified: ${lastModified}`;
+
+// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
+const msToDays = 86400000;
+// today's date
+const theDateToday = new Date();
+
+const lastVisit = localStorage.getItem('lastVisit');
+
+// initialize display elements
+// const todayElement = document.querySelector("#today");
+
+const visitMessageElement = document.querySelector("#visit-message");
+
+// processing
+const today = Date.now();
+
+let message;
+if (!lastVisit) {
+    // First visit
+    message = "Welcome! Let us know if you have any questions.";
+} else {
+    const lastVisitDate = parseInt(lastVisit);
+    const daysBetweenVisits = (today - lastVisitDate) / msToDays;
+
+    if (daysBetweenVisits < 1) {
+        message = "Back so soon! Awesome!";
+    } else {
+        const days = Math.floor(daysBetweenVisits);
+        message = `You last visited ${days} ${days === 1 ? 'day' : 'days'} ago.`;
+    }
+}
+// Display the message
+visitMessageElement.textContent = message;
+
+// Store the current date and time as the last visit date in localStorage
+localStorage.setItem('lastVisit', today);
