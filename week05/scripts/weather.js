34a40543b2ca7240b0624718ea41b6a242512279
geoenterprise <jovannyrey@hotmail.com>
2024-07-10 13:06:25 -0500

    css on weather app

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -5,15 +5,15 @@ const apiKey = "3dd3a034ad96bb287cb4ff6817aaedb8";
 const lat = "49.75";
 const lon = "6.64";
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
 
 async function apiFetch() {
     try {
-      const response = await fetch(apiUrl);
+      const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // testing only
+        // console.log(data); // testing only
         displayResults(data); 
       } else {
           throw Error(await response.text());
@@ -27,7 +27,7 @@ apiFetch();
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
