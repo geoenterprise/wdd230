e847c74478eb95196a51446b99dc4a039d542248
geoenterprise <jovannyrey@hotmail.com>
2024-07-15 17:40:33 -0500

    update fix on index html error

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,6 +1,6 @@
 const currentTemp = document.querySelector('#weather-info');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+// const weatherIcon = document.querySelector('#weather-icon');
+// const captionDesc = document.querySelector('figcaption');
 const apiKey = "3dd3a034ad96bb287cb4ff6817aaedb8";
 const lat = "4.13";
 const lon = "-73.63";
@@ -27,7 +27,7 @@ apiFetch();
 
 function displayResults(data) {
     
-    weatherIcon.innerHTML = "";
+    // weatherIcon.textContent = "";
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     let desc = data.weather[0].description;    
     // weatherIcon.setAttribute('src', iconsrc);
